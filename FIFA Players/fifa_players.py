2d8d9a3839c57a07e595c2281e61e1f01e22d120
fellipejacob <590792@gmail.com>
2021-06-02 16:36:40 -0400

    Feito Boxplot e Scatter de Peso X Altura

--- FIFA Players/fifa_players.py
+++ FIFA Players/fifa_players.py
@@ -10,5 +10,24 @@ dadosselecionados = ['team','height','weight','age','name']
 dadosimportantes = dados.filter(items=dadosselecionados)
 
 #criando um histograma para exibir alguma columa, "bins" são intervalor das colunas
-dadosimportantes.hist(column='height', bins=30)
+# dadosimportantes.hist(column='height', bins=30)
+# plt.show()
+
+#conceito do boxplot:
+#LS(limite superior) = Q3 + 1,5.(Q3-Q1)
+#LI(Limite inferior) = Q1 -1,5.(Q3-Q1)
+#onde Q1 é a mediana dos dados inferiores e Q3 é a mediana dos dados superiores
+
+
+#criando um boxplot para analise dos outliers
+# dadosimportantes.boxplot(column=['age','height','weight'])
+# plt.show()
+
+#criando um novo dataframe usando um teste verifica True or False para o critério:
+brasileiros = dadosimportantes.loc[dadosimportantes['team'] == 'Brazil']
+
+altura = brasileiros['height']
+peso = brasileiros['weight']
+
+plt.scatter(altura,peso)
 plt.show()
