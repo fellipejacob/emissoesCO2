e87ca74188e788e1c2086180300cc37e9834dd98
fellipejacob <590792@gmail.com>
2021-06-02 10:56:47 -0400

    Só temos Enem 2019

---
+++ Enem_Analise_2020/enem_2019.py
@@ -0,0 +1,7 @@
+#analise dos candidatos do enem 2019 para descobrir se o grau de instrução dos pais
+#influencia na nota dos filhos em determinadas matérias.
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('')
